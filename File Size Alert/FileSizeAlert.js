d5e12bf20b3a2fe29dc750d43836dfa50dc6f95a
iJonathan-Dev <48908636+iJonathan-Dev@users.noreply.github.com>
2022-09-01 14:46:00 +1200

    Update FileSizeAlert.js
    optional - add placement div for second text alert

--- File Size Alert/FileSizeAlert.js
+++ File Size Alert/FileSizeAlert.js
@@ -3,7 +3,7 @@
 var jq = jQuery.noConflict(true);
 
 jq("document").ready(function() {
-   // File Size Alert
+//Enrolment File Size Alert
   jq(":file").on('change', function() {
     var totalSize = 0;
     
@@ -13,20 +13,29 @@ jq("document").ready(function() {
       }
     });
    
-    //Change number to maximum file size limit
     var valid = totalSize <= 8388608;
-  
-    //Convert to MB
+
     var fileSize = totalSize / 1048576;
 
     var fileSizeinMB = fileSize.toFixed(2);
 
-    //if exceeding file limit, disabled submit button
-    if (!valid) 
-      alert('Maximum File size limit of 8 MB is exceeded! \nTotal File Size is ' + fileSizeinMB+ ' MB. \n\nTips: please compress the file before uploading.');
     
-    jq(":submit").prop("disabled", !valid);
+ 
+if (!valid){
+        alert('Maximum File size limit of 8 MB is exceeded! \nTotal File Size is ' + fileSizeinMB+ ' MB. \n\nTips: please compress the file before uploading.');
+        jq(":submit").prop("disabled", true);
+      //optional - add placement div for second text alert
+        jq(".textAlert").html("<p><b>Unable to submit, file size limit(8MB) is exceeded! Your total file size is "+ fileSizeinMB+ " MB. Please compress file before uploading.</b></p>");
+  
+      };
+      
+      if (valid){
+         //optional - add placement div for second text alert
+          jq(".textAlert").html("");
+         
+          jq(":submit").prop("disabled", false);
+      }
   });
-})
+});
 
 </script>
